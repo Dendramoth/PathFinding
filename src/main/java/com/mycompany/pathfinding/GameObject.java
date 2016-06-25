744e48ca961c1455cdcba4a1ad6108f658399234
Dendra <Dendra@10.0.0.1>
2016-06-25 21:24:47 +0200

    intesection coordinance detected, corner points of the line touched highlighted

--- src/main/java/com/mycompany/pathfinding/GameObject.java
+++ src/main/java/com/mycompany/pathfinding/GameObject.java
@@ -5,8 +5,11 @@
  */
 package com.mycompany.pathfinding;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
 
@@ -24,7 +27,9 @@ public class GameObject {
     private Polygon gameObjectPolygon = new Polygon();
     private Color color;
     
-    private double[][] pointsToReachEndPolygon;
+    private double[] polygonPoints;
+    
+    private List<Line> polygonLineList = new ArrayList<Line>();
 
     public GameObject(double possitionX, double possitionY, double width, double heigh, GraphicsContext graphicsContext, Color color) {
         this.possitionX = possitionX;
@@ -33,6 +38,11 @@ public class GameObject {
         this.height = heigh;
         this.graphicsContext = graphicsContext;
         this.color = color;
+        
+        polygonLineList.add(new Line(possitionX, possitionY, possitionX + this.width, possitionY));
+        polygonLineList.add(new Line(possitionX + this.width, possitionY, possitionX + this.width, possitionY + height));
+        polygonLineList.add(new Line(possitionX + this.width, possitionY + height, possitionX, possitionY + height));
+        polygonLineList.add(new Line(possitionX, possitionY + height, possitionX, possitionY));
         
         gameObjectPolygon.getPoints().addAll(new Double[]{
             possitionX, possitionY,
@@ -57,6 +67,14 @@ public class GameObject {
 
     public double getPossitionY() {
         return possitionY;
+    }
+
+    public Polygon getGameObjectPolygon() {
+        return gameObjectPolygon;
+    }
+
+    public List<Line> getPolygonLineList() {
+        return polygonLineList;
     }
     
     
--- src/main/java/com/mycompany/pathfinding/Pathfinding.java
+++ src/main/java/com/mycompany/pathfinding/Pathfinding.java
@@ -7,9 +7,12 @@ package com.mycompany.pathfinding;
 
 import java.util.ArrayList;
 import java.util.List;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 /**
@@ -23,6 +26,9 @@ public class Pathfinding {
     private GameObject endGameObject;
     private GraphicsContext graphicsContext;
 
+    private double intersectionXDetected = 0;
+    private double intersectionYDetected = 0;
+
     public Pathfinding(List<GameObject> gameStaticObjectsList, GameObject startGameObject, GameObject endGameObject, GraphicsContext graphicsContext) {
         this.gameStaticObjectsList = gameStaticObjectsList;
         this.startGameObject = startGameObject;
@@ -34,16 +40,42 @@ public class Pathfinding {
         Line line = new Line(startGameObject.getPossitionX(), startGameObject.getPossitionY(), endGameObject.getPossitionX(), endGameObject.getPossitionY());
         graphicsContext.setStroke(Color.RED);
         graphicsContext.strokeLine(startGameObject.getPossitionX(), startGameObject.getPossitionY(), endGameObject.getPossitionX(), endGameObject.getPossitionY());
+
         for (GameObject gameStaticObject : gameStaticObjectsList) {
             Shape intersection = gameStaticObject.detectIntersection(line);
             if (intersection.getLayoutBounds().getHeight() <= 0 || intersection.getLayoutBounds().getWidth() <= 0) {
             } else {
-                System.out.println("------------------------");
-                System.out.println(intersection.getLayoutBounds().getMinX());
-                System.out.println(intersection.getLayoutBounds().getMinY());
-                graphicsContext.fillOval(intersection.getLayoutBounds().getMinX() - 5, intersection.getLayoutBounds().getMinY() - 5, 10, 10);
+                getIntersectionPointCoordinates(intersection);
+                graphicsContext.fillOval(intersectionXDetected - 5, intersectionYDetected - 5, 11, 11);
+
+                findPolygonTouchedLineCorners(gameStaticObject, new Rectangle(intersectionXDetected - 1, intersectionYDetected - 1, 3, 3));
             }
 
+        }
+    }
+
+    private void getIntersectionPointCoordinates(Shape intersection) {
+        if (startGameObject.getPossitionX() < endGameObject.getPossitionX()) {
+            intersectionXDetected = intersection.getLayoutBounds().getMinX();
+        } else {
+            intersectionXDetected = intersection.getLayoutBounds().getMaxX();
+        }
+
+        if (startGameObject.getPossitionY() < endGameObject.getPossitionY()) {
+            intersectionYDetected = intersection.getLayoutBounds().getMinY();
+        } else {
+            intersectionYDetected = intersection.getLayoutBounds().getMaxY();
+        }
+    }
+
+    private void findPolygonTouchedLineCorners(GameObject gameObject, Shape shape) {
+        for (Line line : gameObject.getPolygonLineList()) {
+            Shape intersection = Shape.intersect(line, shape);
+            if (!(intersection.getLayoutBounds().getHeight() <= 0 || intersection.getLayoutBounds().getWidth() <= 0)) {
+                graphicsContext.fillOval(line.getEndX() - 5, line.getEndY() - 5, 10, 10);
+                graphicsContext.fillOval(line.getStartX() - 5, line.getStartY() - 5, 10, 10);
+                break;
+            }
         }
     }
 
